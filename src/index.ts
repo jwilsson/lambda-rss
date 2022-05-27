b9fa424d3da2fd3d2c56aeac6d1c685f738e2d1c
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2022-05-27 14:47:49 +0200

    Move response creation to its own function

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,18 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { sites } from './sites';
+import rss from './rss';
 
 export const handler = async ({
     queryStringParameters,
 }: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const { s = '' } = queryStringParameters ?? {};
-    const site = sites.get(s);
-
-    if (!site) {
-        return {
-            body: '',
-            isBase64Encoded: false,
-            statusCode: 400,
-        };
-    }
-
-    const { fetch, name, url } = site;
-    const posts = await fetch();
-    const body = `
-        <?xml version="1.0" encoding="UTF-8" ?>
-        <rss version="2.0">
-            <channel>
-                <title>${name}</title>
-                <description>${name}</description>
-                <link>${url}</link>
-                <ttl>720</ttl>
-                ${posts
-                    .map(
-                        (post) => `
-                    <item>
-                        <title>${post.title}</title>
-                        <description>${post.description}</description>
-                        <guid>${post.link}</guid>
-                        <link>${post.link}</link>
-                        <pubDate>${post.date}</pubDate>
-                    </item>
-                `,
-                    )
-                    .join('')}
-            </channel>
-        </rss>
-    `.trim();
+    const body = await rss(s);
 
     return {
         body,
         headers: {
-            'Content-Type': 'application/rss+xml',
+            'Content-Type': body ? 'application/rss+xml' : 'text/plain',
         },
         isBase64Encoded: false,
-        statusCode: 200,
+        statusCode: body ? 200 : 400,
     };
 };
